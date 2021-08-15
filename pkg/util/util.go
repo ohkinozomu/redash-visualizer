5f738299cf7a46278458b7b0d96974281485a21d
ohkinozomu <nozomunoise@gmail.com>
2021-08-15 23:15:08 +0900

    Add logging

---
+++ pkg/util/util.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"strings"
+
+	"github.com/snowplow-devops/redash-client-go/redash"
+)
+
+func JoinDataSources(ds *[]redash.DataSource) string {
+	dataSources := ""
+	for _, v := range *ds {
+		dataSources += v.Name
+		dataSources += ","
+	}
+	return strings.TrimRight(dataSources, ",")
+}
+
+func JoinGroups(groups *[]redash.Group) string {
+	g := ""
+	for _, v := range *groups {
+		g += v.Name
+		g += ","
+	}
+	return strings.TrimRight(g, ",")
+}
+
+func JoinUsers(users *redash.UserList) string {
+	u := ""
+	for _, v := range users.Results {
+		u += v.Name
+		u += ","
+	}
+	return strings.TrimRight(u, ",")
+}
